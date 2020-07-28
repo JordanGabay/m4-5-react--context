2fb8b2b9a8ae0ae73529cc069a00281e0edaa1c3
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-28 15:15:15 -0400

    Exercise 1 Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,58 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import usePersistedState from '../hooks/usePersistedState.hook';
+import { createContext } from 'react';
+import { items } from '../components/Data'
 import GlobalStyles from "./GlobalStyles";
 import Home from "./Home";
 import Game from "./Game";
 
-function App(props) {
+export const AppContext = createContext();
+
+const App = () => {
+  const [numCookies, setNumCookies] = usePersistedState("num-cookies", 1000);
+  const [purchasedItems, setPurchasedItems] = usePersistedState("items", {
+    cursor: 0,
+    grandma: 0,
+    farm: 0,
+  });
+
+  const incrementCookies = () => {
+    setNumCookies((c) => c + 1);
+  };
+
+  const calculateCookiesPerSecond = (purchasedItems) => {
+    return Object.keys(purchasedItems).reduce((acc, itemId) => {
+      const numOwned = purchasedItems[itemId];
+      const item = items.find((item) => item.id === itemId);
+      const value = item.value;
+
+      return acc + value * numOwned;
+    }, 0);
+  };
+  
   return (
-    <>
+    <AppContext.Provider
+      value={{
+        numCookies,
+        setNumCookies,
+        purchasedItems,
+        setPurchasedItems,
+        incrementCookies,
+        calculateCookiesPerSecond,
+      }}
+    >
       <GlobalStyles />
       <Router>
         <Route exact path="/">
           <Home />
         </Route>
         <Route path="/game">
-          <Game />
+          <Game/>
         </Route>
       </Router>
-    </>
+      </AppContext.Provider>
   );
-}
+};
 
 export default App;
